35b8e53a8aae07faa091efa1b7fca1095bc9a56d
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2010-02-26 09:41:26 +0000

    tagged 3.4.99

---
+++ 3.5/src/setup.py
@@ -0,0 +1,113 @@
+from distutils.core import setup
+import py2exe, sys, os, shutil, glob
+
+for d in ('build', 'dist'):
+	if os.path.exists(d):
+		shutil.rmtree(d)
+
+# If run without args, build executables, in quiet mode.
+if (len(sys.argv) == 1):
+	sys.argv.append("py2exe")
+	sys.argv.append("-q")
+
+def tree(src):
+	list = [(root, map(lambda f: os.path.join(root, f), files)) for (root, dirs, files) in os.walk(os.path.normpath(src))]
+	new_list = []
+	for (root, files) in list:
+		if (len(files) > 0) and (root.count('.svn') == 0):
+			new_list.append((root, files))
+	return new_list
+
+class Target:
+	def __init__(self, **kw):
+		self.__dict__.update(kw)
+		self.company_name = "uib GmbH"
+		self.copyright = "uib GmbH"
+		self.version = ""
+		f = open(self.script, 'r')
+		for line in f.readlines():
+			if (line.find("__version__") != -1):
+				self.version = line.split('=', 1)[1].strip()[1:-1]
+				break
+		f.close()
+		if not self.version:
+			print >> sys.stderr, "Failed to find version of script '%s'" % self.script
+
+opsiclientd = Target(
+	name = "opsiclientd",
+	description = "opsi client daemon",
+	script = "opsiclientd.py",
+	modules = ["opsiclientd"],
+	#cmdline_style='pywin32',
+)
+
+notifier = Target(
+	name = "notifier",
+	description = "opsi notifier",
+	script = "windows\\helpers\\notifier\\notifier.py",
+)
+
+opsiclientd_rpc = Target(
+	name = "opsiclientd_rpc",
+	description = "opsi client daemon rpc tool",
+	script = "windows\\helpers\\opsiclientd_rpc\\opsiclientd_rpc.py",
+)
+
+action_processor_starter = Target(
+	name = "action_processor_starter",
+	description = "opsi action processor starter",
+	script = "windows\\helpers\\action_processor_starter\\action_processor_starter.py",
+)
+################################################################
+# COM pulls in a lot of stuff which we don't want or need.
+
+excludes = [	"pywin", "pywin.debugger", "pywin.debugger.dbgcon",
+		"pywin.dialogs", "pywin.dialogs.list",
+		"Tkconstants", "Tkinter", "tcl", "_imagingtk",
+		"PIL._imagingtk", "ImageTk", "PIL.ImageTk", "FixTk"
+]
+
+data_files = [
+	('Microsoft.VC90.MFC', glob.glob('Microsoft.VC90.MFC\*.*')),
+	('Microsoft.VC90.CRT', glob.glob('Microsoft.VC90.CRT\*.*')),
+	('lib\\Microsoft.VC90.MFC', glob.glob('Microsoft.VC90.MFC\*.*')),
+	('lib\\Microsoft.VC90.CRT', glob.glob('Microsoft.VC90.CRT\*.*')),
+	('notifier',                      [	'windows\\helpers\\notifier\\event.ini',
+						'windows\\helpers\\notifier\\action.ini',
+						'windows\\helpers\\notifier\\userlogin.ini',
+						'windows\\helpers\\notifier\\wait_for_gui.ini',
+						'windows\\helpers\\notifier\\block_login.ini',
+						'windows\\helpers\\notifier\\event.bmp',
+						'windows\\helpers\\notifier\\action.bmp',
+						'windows\\helpers\\notifier\\userlogin.bmp',
+						'windows\\helpers\\notifier\\wait_for_gui.bmp',
+						'windows\\helpers\\notifier\\block_login.bmp',
+						'windows\\helpers\\notifier\\opsi.ico']),
+	('opsiclientd',                   [	'windows\\opsiclientd.conf']),
+	('opsiclientd\\static_html',      [	'..\\static_html\\favicon.ico', '..\\static_html\\index.html', '..\\static_html\\opsi_logo.png']),
+	('opsiclientd\\backendManager.d', [	'..\\cache_service.conf'])
+]
+#data_files += tree("static_html")
+
+setup(
+	options = {
+		"py2exe": {
+			"compressed": 1,
+			#"bundle_files": 1,
+			"optimize": 2,
+			"excludes": excludes,
+			"packages": ["OPSI", "twisted"]
+		}
+	},
+	data_files = data_files,
+	zipfile = "lib/library.zip",
+	service = [ opsiclientd ],
+	windows = [ notifier, opsiclientd_rpc, action_processor_starter ],
+)
+
+#os.unlink(os.path.join("dist", "w9xpopen.exe"))
+
+print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+print "!!!   On the target machine always replace exe AND lib   !!!"
+print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
---
+++ 3.5/src/windows/helpers/opsiclientd_rpc/opsiclientd_rpc.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+   = = = = = = = = = = = = = = = = = = = = =
+   =            opsiclientd_rpc            =
+   = = = = = = = = = = = = = = = = = = = = =
+   
+   opsiclientd_rpc is part of the desktop management solution opsi
+   (open pc server integration) http://www.opsi.org
+   
+   Copyright (C) 2010 uib GmbH
+   
+   http://www.uib.de/
+   
+   All rights reserved.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2 as
+   published by the Free Software Foundation.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+   
+   @copyright:	uib GmbH <info@uib.de>
+   @author: Jan Schneider <j.schneider@uib.de>
+   @license: GNU General Public License version 2
+"""
+
+__version__ = '3.5'
+
+# Imports
+import sys, os, locale
+
+from OPSI import System
+from OPSI.Backend.JSONRPC import JSONRPCBackend
+
+encoding = locale.getpreferredencoding()
+argv = [ unicode(arg, encoding) for arg in sys.argv ]
+
+if (len(argv) != 5):
+	print u"Usage: %s <username> <password> <port> <rpc>" % os.path.basename(argv[0])
+	sys.exit(1)
+
+(username, password, port, rpc) = argv[1:]
+try:
+	be = JSONRPCBackend(username = username, password = password, address = u'https://localhost:%s/opsiclientd' % port)
+	exec 'be.%s' % rpc
+	be.backend_exit()
+except:
+	pass
+sys.exit(0)
+
