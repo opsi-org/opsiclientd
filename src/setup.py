668a1304700b213123e2d4c2b6eb64d444bde165
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2010-06-17 15:26:03 +0000

    -

--- src/setup.py
+++ src/setup.py
@@ -41,10 +41,38 @@ opsiclientd = Target(
 	#cmdline_style='pywin32',
 )
 
+# manifest_template = '''
+# <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+# <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+# <assemblyIdentity
+#     version="5.0.0.0"
+#     processorArchitecture="x86"
+#     name="%(prog)s"
+#     type="win32"
+# />
+# <description>%(prog)s Program</description>
+# <dependency>
+#     <dependentAssembly>
+#         <assemblyIdentity
+#             type="win32"
+#             name="Microsoft.Windows.Common-Controls"
+#             version="6.0.0.0"
+#             processorArchitecture="X86"
+#             publicKeyToken="6595b64144ccf1df"
+#             language="*"
+#         />
+#     </dependentAssembly>
+# </dependency>
+# </assembly>
+# '''
+# 
+# RT_MANIFEST = 24
+
 notifier = Target(
 	name = "notifier",
 	description = "opsi notifier",
 	script = "windows\\helpers\\notifier\\notifier.py",
+	# other_resources = [(RT_MANIFEST, 1, manifest_template % dict(prog="notifier"))],
 )
 
 opsiclientd_rpc = Target(
