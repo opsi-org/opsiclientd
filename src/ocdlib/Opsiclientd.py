9ca91062f711bb6707e62bba579885edac3a34e4
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2010-07-21 13:33:29 +0000

    develop

--- src/ocdlib/Opsiclientd.py
+++ src/ocdlib/Opsiclientd.py
@@ -1263,15 +1263,27 @@ class EventProcessingThread(KillableThread):
 		if self._configService.isLegacyOpsi():
 			return
 		
+		self._configService.backend_setOptions({"addConfigStateDefaults": True})
+		
+		dynamicDepot = False
+		for configState in self._configService.configState_getObjects(configId = 'clientconfig.depot.dynamic', objectId = self.opsiclientd.getConfigValue('global', 'host_id')):
+			if configState.values:
+				dynamicDepot = forceBool(configState.values[0])
+				break
+		
+		if dynamicDepot:
+			logger.info(u"Dynamic depot selection enabled")
+		
 		clientToDepotservers = self._configService.configState_getClientToDepotserver(
 				clientIds  = [ self.opsiclientd.getConfigValue('global', 'host_id') ],
-				masterOnly = False,
+				masterOnly = (not dynamicDepot),
 				productIds = productIds)
 		if not clientToDepotservers:
 			raise Exception(u"Failed to get depot config from service")
 		
 		depotIds = [ clientToDepotservers[0]['depotId'] ]
-		depotIds.extend(clientToDepotservers[0].get('slaveDepotIds', []))
+		if dynamicDepot:
+			depotIds.extend(clientToDepotservers[0].get('slaveDepotIds', []))
 		masterDepot = None
 		slaveDepots = []
 		for depot in self._configService.host_getObjects(type = 'OpsiDepotserver', id = depotIds):
@@ -1285,8 +1297,38 @@ class EventProcessingThread(KillableThread):
 		self.opsiclientd.setConfigValue('depot_server', 'depot_id', masterDepot.id)
 		self.opsiclientd.setConfigValue('depot_server', 'url', masterDepot.depotRemoteUrl)
 		
-		if slaveDepots:
+		if dynamicDepot and slaveDepots:
 			try:
+				modules = serviceConnectionThread.configService.backend_info()['modules']
+			
+				if not modules.get('dynamic_depot'):
+					raise Exception(u"Dynamic depot module currently disabled")
+				
+				if not modules.get('customer'):
+					raise Exception(u"No customer in modules file")
+					
+				if not modules.get('valid'):
+					raise Exception(u"Modules file invalid")
+				
+				if (modules.get('expires', '') != 'never') and (time.mktime(time.strptime(modules.get('expires', '2000-01-01'), "%Y-%m-%d")) - time.time() <= 0):
+					raise Exception(u"Modules file expired")
+				
+				logger.info(u"Verifying modules file signature")
+				publicKey = keys.Key.fromString(data = base64.decodestring('AAAAB3NzaC1yc2EAAAADAQABAAABAQCAD/I79Jd0eKwwfuVwh5B2z+S8aV0C5suItJa18RrYip+d4P0ogzqoCfOoVWtDojY96FDYv+2d73LsoOckHCnuh55GA0mtuVMWdXNZIE8Avt/RzbEoYGo/H0weuga7I8PuQNC/nyS8w3W8TH4pt+ZCjZZoX8S+IizWCYwfqYoYTMLgB0i+6TCAfJj3mNgCrDZkQ24+rOFS4a8RrjamEz/b81noWl9IntllK1hySkR+LbulfTGALHgHkDUlk0OSu+zBPw/hcDSOMiDQvvHfmR4quGyLPbQ2FOVm1TzE0bQPR+Bhx4V8Eo2kNYstG2eJELrz7J1TJI0rCjpB+FQjYPsP')).keyObject
+				data = u''
+				mks = modules.keys()
+				mks.sort()
+				for module in mks:
+					if module in ('valid', 'signature'):
+						continue
+					val = modules[module]
+					if (val == False): val = 'no'
+					if (val == True):  val = 'yes'
+					data += u'%s = %s\r\n' % (module.lower().strip(), val)
+				if not bool(publicKey.verify(md5(data).digest(), [ long(modules['signature']) ])):
+					raise Exception(u"Modules file invalid")
+				logger.notice(u"Modules file signature verified (customer: %s)" % modules.get('customer'))
+				
 				defaultInterface = None
 				networkInterfaces = System.getNetworkInterfaces()
 				if not networkInterfaces:
