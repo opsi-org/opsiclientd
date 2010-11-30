863c3fe882db30e0589b11841bbc458704764b48
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2010-11-30 10:18:26 +0000

    -

---
+++ create_archive.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+destdir=$1
+cwd=$(pwd)
+dir=$(dirname ${cwd}/$(dirname $0))
+[ "$destdir" = "" ] && destdir=$cwd
+
+cd $dir
+
+packagename="opsiclientd"
+version=$(grep '^__version__' src/ocdlib/Opsiclientd.py | head -n1 | cut -d'=' -f2 | sed s"/\s*'//g")
+tmpdir=/tmp/${packagename}-${version}
+
+test -e $tmpdir && rm -rf $tmpdir
+mkdir $tmpdir
+cp -r gettext static_html ${tmpdir}/
+mkdir ${tmpdir}/src
+cp -r src/Microsoft.VC90.CRT src/Microsoft.VC90.MFC src/ocdlib src/windows src/setup.py ${tmpdir}/src
+
+find ${tmpdir} -iname "*.pyc"   -exec rm "{}" \;
+find ${tmpdir} -iname "*.marks" -exec rm "{}" \;
+find ${tmpdir} -iname "*~"      -exec rm "{}" \;
+find ${tmpdir} -iname "*.svn"   -exec rm -rf "{}" \; 2>/dev/null
+
+
+cd ${tmpdir}/..
+tar cjvf ${destdir}/${packagename}-${version}.tar.bz2 ${packagename}-${version}
+rm -rf $tmpdir
+echo "============================================================================================="
+echo "source archive: ${destdir}/${packagename}_${version}.tar.bz2"
+echo "============================================================================================="
+cd $cwd
+
