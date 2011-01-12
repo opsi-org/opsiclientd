39262d4e294bddbc361181a42078273bc48b73bc
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2011-01-12 13:03:11 +0000

    -

---
+++ src/ocdlibnonfree/SoftwareOnDemand.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+   = = = = = = = = = = = = = = = = = = =
+   =   ocdlibnonfree                   =
+   = = = = = = = = = = = = = = = = = = =
+   
+   This module is part of the desktop management solution opsi
+   (open pc server integration) http://www.opsi.org
+   
+   Copyright (C) 2010 uib GmbH
+   
+   http://www.uib.de/
+   
+   All rights reserved.
+   
+   @copyright:	uib GmbH <info@uib.de>
+   @author: Erol Ülükmen <e.ueluekmen@uib.de>
+"""
+
+
+# OPSI imports
+from OPSI.web2 import responsecode, http, stream
+from OPSI.Logger import *
+from OPSI.Config import Config
+from OPSI.Types import *
+from OPSI.Service.Worker import WorkerOpsi
+from OPSI.Service.Resource import ResourceOpsi
+
+from ocdlib.OpsiService import ServiceConnection
+
+logger = Logger()
+config = Config()
+
+kioskPage = u'''
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+</head>
+<body>
+%result%
+</body>
+'''
+
+class WorkerSoftwareOnDemand(WorkerOpsi, ServiceConnection):
+	def __init__(self, service, request, resource):
+		moduleName = u' %-30s' % (u'software on demand')
+		logger.setLogFormat(u'[%l] [%D] [' + moduleName + u'] %M   (%F|%N)', object=self)
+		WorkerOpsi.__init__(self, service, request, resource)
+		ServiceConnection.__init__(self)
+		
+	def _getCredentials(self):
+		(user, password) = self._getAuthorization()
+		if not user:
+			user = config.get('global', 'host_id')
+		return (user, password)
+		
+	def _authenticate(self, result):
+		if self.session.authenticated:
+			return result
+		if (self.request.remoteAddr.host == '127.0.0.1'):
+			return result
+		try:
+			(self.session.user, self.session.password) = self._getCredentials()
+			
+			logger.notice(u"Authorization request from %s@%s (application: %s)" % (self.session.user, self.session.ip, self.session.userAgent))
+			
+			if not self.session.password:
+				raise Exception(u"No password from %s (application: %s)" % (self.session.ip, self.session.userAgent))
+			
+			if (self.session.user.lower() == config.get('global', 'host_id').lower()) and (self.session.password == config.get('global', 'opsi_host_key')):
+				return result
+			if (os.name == 'nt'):
+				if (self.session.user.lower() == 'administrator'):
+					import win32security
+					# The LogonUser function will raise an Exception on logon failure
+					win32security.LogonUser(self.session.user, 'None', self.session.password, win32security.LOGON32_LOGON_NETWORK, win32security.LOGON32_PROVIDER_DEFAULT)
+					# No exception raised => user authenticated
+					return result
+			
+			raise Exception(u"Invalid credentials")
+		except Exception, e:
+			raise OpsiAuthenticationError(u"Forbidden: %s" % forceUnicode(e))
+		return result
+	
+	def _processQuery(self, result):
+		self._decodeQuery(self, result)
+		
+	def _generateResponse(self, result):
+		self.connectConfigService()
+		self._configService
+		productIds = []
+		for objectToGroup in self._configService.objectToGroup_getObjects(groupType = "ProductGroup", groupId = "kiosk"):
+			productIds.append(objectToGroup.objectId)
+		
+		html = kioskPage
+		html = html.replace('%result%', forceUnicode(productIds))
+		
+		if not isinstance(result, http.Response):
+			result = http.Response()
+		
+		result.code = responsecode.OK
+		#result.stream = stream.IByteStream((u'Kiosk ' + self.query).encode('utf-8'))
+		result.stream = stream.IByteStream(html.encode('utf-8'))
+		return result
+	
+
+class ResourceSoftwareOnDemand(ResourceOpsi):
+	WorkerClass = WorkerSoftwareOnDemand
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
