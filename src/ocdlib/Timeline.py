833e3a55a801e9fbe8b90aac07f5f4c139e9cd64
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2011-02-03 13:53:56 +0000

    -

--- src/ocdlib/Timeline.py
+++ src/ocdlib/Timeline.py
@@ -54,8 +54,8 @@ Timeline_ajax_url   = "/timeline/timeline_ajax/simile-ajax-api.js";
 Timeline_urlPrefix  = "/timeline/timeline_js/";
 Timeline_parameters = "bundle=true";
 </script>
-<script src="/timeline/timeline_js/timeline-api.js" type="text/javascript"></script>
-<script src="/timeline/timeline_ajax/simile-ajax-api.js" type="text/javascript"></script>
+<script src="/timeline/timeline_js/timeline-api.js?bundle=true" type="text/javascript"></script>
+<script src="/timeline/timeline_ajax/simile-ajax-api.js?bundle=true" type="text/javascript"></script>
 <script>
 var timeline_data = %(data)s;
 var tl;
