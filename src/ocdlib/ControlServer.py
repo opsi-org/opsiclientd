ce5b3b6fce53af9ab7e769c086711256ccfc95e4
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2011-02-18 14:34:12 +0000

    -

--- src/ocdlib/ControlServer.py
+++ src/ocdlib/ControlServer.py
@@ -84,7 +84,7 @@ infoPage = u'''
 	</span>
 	
 	<div class="infopage-timeline-box">
-		<p class="infopage-timeline-title">Timeline:</p>
+		<p class="infopage-timeline-title">Timeline</p>
 		<div class="timeline-default" id="opsiclientd-timeline" style="height: 400px; border: 1px solid #aaaaaa"></div>
 		<noscript>
 		This page uses Javascript to show you a Timeline. Please enable Javascript in your browser to see the full page. Thank you.
