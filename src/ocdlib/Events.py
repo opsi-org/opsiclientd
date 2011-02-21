aaa9655bb8f40b05574ee05f1c8f2b6777bfe44b
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2011-02-21 14:11:29 +0000

    -

--- src/ocdlib/Events.py
+++ src/ocdlib/Events.py
@@ -183,7 +183,7 @@ class EventConfig(object):
 		return self.name
 	
 	def getActionMessage(self):
-		actionMessage = self.actionMessage
+		message = self.actionMessage
 		def toUnderscore(value):
 			s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', value)
 			return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
