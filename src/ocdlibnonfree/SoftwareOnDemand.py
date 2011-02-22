8a4ee78af5b5eb109ba60f298a7d5dbec1619b8d
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2011-02-22 13:44:55 +0000

    -

--- src/ocdlibnonfree/SoftwareOnDemand.py
+++ src/ocdlibnonfree/SoftwareOnDemand.py
@@ -39,13 +39,13 @@ logger = Logger()
 config = Config()
 timeline = Timeline()
 
-mainpage = u'''
-<?xml version="1.0" encoding="UTF-8"?>
+mainpage = u'''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd" />
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>opsi Software On Demand</title>
 	<link rel="stylesheet" type="text/css" href="/opsiclientd.css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 	<span id="title">
