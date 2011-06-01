4186a716395537d75d7ee310a01dbbbef4d874ea
schneider <schneider@b677a167-1dbb-43b9-94d8-85d86bfab79e>
2011-06-01 11:57:41 +0000

    root ca

--- src/ocdlib/OpsiService.py
+++ src/ocdlib/OpsiService.py
@@ -54,6 +54,34 @@ from ocdlib.Exceptions import *
 logger = Logger()
 config = Config()
 
+
+OPSI_CA = '''-----BEGIN CERTIFICATE-----
+MIIEYTCCA0mgAwIBAgIJAO5oKZZR8dQkMA0GCSqGSIb3DQEBBQUAMH0xCzAJBgNV
+BAYTAkRFMRgwFgYDVQQIEw9SaGVpbmxhbmQtUGZhbHoxDjAMBgNVBAcTBU1haW56
+MREwDwYDVQQKEwh1aWIgR21iSDEVMBMGA1UEAxMMb3BzaSBSb290IENBMRowGAYJ
+KoZIhvcNAQkBFgtpbmZvQHVpYi5kZTAeFw0xMTA2MDExMDI2NTNaFw0yMTA1Mjkx
+MDI2NTNaMH0xCzAJBgNVBAYTAkRFMRgwFgYDVQQIEw9SaGVpbmxhbmQtUGZhbHox
+DjAMBgNVBAcTBU1haW56MREwDwYDVQQKEwh1aWIgR21iSDEVMBMGA1UEAxMMb3Bz
+aSBSb290IENBMRowGAYJKoZIhvcNAQkBFgtpbmZvQHVpYi5kZTCCASIwDQYJKoZI
+hvcNAQEBBQADggEPADCCAQoCggEBAJxU7TXeNrwXPlsermmdRxvPkzaNqE7q9oev
+lTLrdzMFNXekpg7nTMdvMEcPezHgkxrzRnIFrbyCKebVHvYBMYDSMefL0PGdBufW
+vRuQVH5VtdjCZ3SJWHjrLHeV4RCddS/5f1Mx9mxaXuO/0qtpttFQKQ7wHU5a/8eE
+Y2P+ZY7K4s8E/ZA2V3Tu6HxZJIt/JG0HoGrgEShb5hhRlpTVP5gRl/14qaZp9JZq
+Hn7UHMEJlWLb7EXzY7wRIiHmI//V69X9ARrkS5axbDddatlZBEGonSfgObna5YOO
+6Lx5aiq/PyYMEA6YWG+le//KgxexLCf5t5i8PEiFLuBrXCrKDG0CAwEAAaOB4zCB
+4DAdBgNVHQ4EFgQUYr4TyTM6odj+hYr3luci6pnFtzEwgbAGA1UdIwSBqDCBpYAU
+Yr4TyTM6odj+hYr3luci6pnFtzGhgYGkfzB9MQswCQYDVQQGEwJERTEYMBYGA1UE
+CBMPUmhlaW5sYW5kLVBmYWx6MQ4wDAYDVQQHEwVNYWluejERMA8GA1UEChMIdWli
+IEdtYkgxFTATBgNVBAMTDG9wc2kgUm9vdCBDQTEaMBgGCSqGSIb3DQEJARYLaW5m
+b0B1aWIuZGWCCQDuaCmWUfHUJDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUA
+A4IBAQCFSalk9ngRf+03YW6StULDkuMSRF6oj1A5J3eRZzXTL1uckTseXm5CK13d
+OgxjZtgzD/TiVWoOmxGPVA+YYjLKpVUpPWu6opAG8cy705MeNxfAHLj+mn+joAxn
+qjjH46t2W6hdcz0x86bIVSda97/erARX8ALBreI3e3iIH9D2de8IH5uj6q0UTO/P
+YJHaSeCITO1g+NXisCS/aEfL+yUjXjErQaiRjtyj0aHDxj114GVvbKUOUfHqqa6X
+USZQNXthwmMy0+iIgQLAmBDu9Tz53p+yqHIhS+7eYNfzh2HeIG3EY515ncnZG2Xi
+QuBW/YzuIIiknjESIHBVA6YWeLNR
+-----END CERTIFICATE-----'''
+
 def isConfigServiceReachable(timeout=5):
 	for url in config.getConfigServiceUrls():
 		try:
@@ -282,11 +310,18 @@ class ServiceConnectionThread(KillableThread):
 			verifyServerCert = config.get('global', 'verify_server_cert')
 			if not os.path.exists(certDir):
 				os.makedirs(certDir)
-			
+				
 			(scheme, host, port, baseurl, username, password) = urlsplit(self._configServiceUrl)
 			serverCertFile = os.path.join(certDir, host + '.pem')
 			if verifyServerCert:
 				logger.info(u"Server verification enabled, using cert file '%s'" % serverCertFile)
+			
+			verifyByCaCertsFile = None
+			if config.get('global', 'verify_server_cert_by_ca'):
+				verifyByCaCertsFile = os.path.join(certDir, 'cacert.pem')
+				f = open(verifyByCaCertsFile, 'w')
+				f.write(OPSI_CA)
+				f.close()
 			
 			tryNum = 0
 			while not self.cancelled and not self.connected:
@@ -297,11 +332,12 @@ class ServiceConnectionThread(KillableThread):
 					if (len(self._username.split('.')) < 3):
 						raise Exception(u"Domain missing in username '%s'" % self._username)
 					self.configService = JSONRPCBackend(
-						address          = self._configServiceUrl,
-						username         = self._username,
-						password         = self._password,
-						serverCertFile   = serverCertFile,
-						verifyServerCert = verifyServerCert,
+						address             = self._configServiceUrl,
+						username            = self._username,
+						password            = self._password,
+						serverCertFile      = serverCertFile,
+						verifyServerCert    = verifyServerCert,
+						verifyByCaCertsFile = verifyByCaCertsFile,
 						application = 'opsiclientd version %s' % __version__)
 					if self.configService.isLegacyOpsi():
 						self.configService.authenticated()
