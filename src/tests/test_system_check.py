9d465ebb3b3fd1654212ae5b06d915d63a011552
Niko Wenselowski <n.wenselowski@uib.de>
2013-09-09 15:27:39 +0000

    First test.

---
+++ src/tests/test_system_check.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+from context import SystemCheck as syscheck
+
+
+class Test(unittest.TestCase):
+    def test_system_determining(self):
+        if os.name == 'nt':
+            self.assertTrue(syscheck.RUNNING_ON_WINDOWS)
+        else:
+            self.assertFalse(syscheck.RUNNING_ON_WINDOWS)
+
+
+if __name__ == '__main__':
+    unittest.main()
