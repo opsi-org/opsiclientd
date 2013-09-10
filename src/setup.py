a76fd24311ed6cb8dbe483ccaaa7e0de6345dfa3
Niko Wenselowski <n.wenselowski@uib.de>
2013-09-10 08:19:16 +0000

    Modified setup.py to work with Windows and Linux.

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,20 @@
-from distutils.core import setup
-import py2exe, sys, os, shutil, glob
+# -*- coding: utf-8 -*-
 
-for d in ('build', 'dist'):
-	if os.path.exists(d):
-		shutil.rmtree(d)
+import glob
+import os
+import shutil
+import sys
+from distutils.core import setup
+
+
+RUNS_ON_WINDOWS = (sys.platform == 'nt')
+
+if RUNS_ON_WINDOWS:
+	import py2exe
+
+for directory in ('build', 'dist'):
+	if os.path.exists(directory):
+		shutil.rmtree(directory)
 
 buildFreeVersion = False
 if '--free' in sys.argv:
@@ -11,48 +22,63 @@ if '--free' in sys.argv:
 	sys.argv.remove('--free')
 
 # If run without args, build executables, in quiet mode.
-if (len(sys.argv) == 1):
+if RUNS_ON_WINDOWS and len(sys.argv) == 1:
 	sys.argv.append("py2exe")
 	sys.argv.append("-q")
 
+opsiClientDeamonVersion = None
+try:
+	with open(os.path.join('ocdlib', 'Opsiclientd.py'), 'r') as f:
+		for line in f:
+			if "__version__" in line:
+				opsiClientDeamonVersion = line.split('=', 1)[1].strip()[1:-1]
+				break
+
+	if opsiClientDeamonVersion is None:
+		raise Exception("Failed to find version.")
+except Exception:
+	print 'Unable to determine version.'
+	opsiClientDeamonVersion = '0.0.0a'
+
+
 def tree(dst, src):
-	list = []
+	unwanted_directories = ('.svn', '.git')
+	found_files = []
 	for (root, dirs, files) in os.walk(os.path.normpath(src)):
-		if '.svn' in (dirs):
-			dirs.remove('.svn')
-		if root.endswith('.svn'):
-			continue
+		for unwanted_directory in unwanted_directories:
+			if unwanted_directory in dirs:
+				dirs.remove(unwanted_directory)
+
+		for unwanted_directory in unwanted_directories:
+			if root.endswith(unwanted_directory):
+				continue
+
 		newfiles = []
 		for f in files:
 			if f.endswith('~'):
 				continue
 			newfiles.append(os.path.join(root, f))
+
 		if not newfiles:
 			continue
-		list.append( (os.path.normpath(os.path.join(dst, root)), newfiles) )
-	return list
+		found_files.append( (os.path.normpath(os.path.join(dst, root)), newfiles) )
+	return found_files
+
 
 class Target:
 	def __init__(self, **kw):
 		self.__dict__.update(kw)
 		self.company_name = "uib GmbH"
 		self.copyright = "uib GmbH"
-		self.version = ""
-		f = open(os.path.join('ocdlib', 'Opsiclientd.py'), 'r')
-		for line in f.readlines():
-			if (line.find("__version__") != -1):
-				self.version = line.split('=', 1)[1].strip()[1:-1]
-				break
-		f.close()
-		if not self.version:
-			print >> sys.stderr, "Failed to find version of script '%s'" % self.script
+		self.version = opsiClientDeamonVersion
+
 
 opsiclientdDescription = "opsi client daemon"
 packages = ["OPSI", "twisted", "ocdlib"]
-excludes = [	"pywin", "pywin.debugger", "pywin.debugger.dbgcon",
-		"pywin.dialogs", "pywin.dialogs.list",
-		"Tkconstants", "Tkinter", "tcl", "_imagingtk",
-		"PIL._imagingtk", "ImageTk", "PIL.ImageTk", "FixTk"
+excludes = [
+	"pywin", "pywin.debugger", "pywin.debugger.dbgcon",	"pywin.dialogs",
+	"pywin.dialogs.list", "Tkconstants", "Tkinter", "tcl", "_imagingtk",
+	"PIL._imagingtk", "ImageTk", "PIL.ImageTk", "FixTk"
 ]
 
 if os.path.exists("ocdlibnonfree") and not buildFreeVersion:
@@ -61,126 +87,155 @@ if os.path.exists("ocdlibnonfree") and not buildFreeVersion:
 else:
 	excludes.append("ocdlibnonfree")
 
-print "Building %s" % opsiclientdDescription
+print "Building %s (Version %s)" % (opsiclientdDescription, opsiClientDeamonVersion)
 
-opsiclientd = Target(
-	name = "opsiclientd",
-	description = opsiclientdDescription,
-	script = "opsiclientd.py",
-	modules = ["opsiclientd"],
-	#cmdline_style='pywin32',
-	icon_resources = [(1, "windows\\opsi.ico")]
-)
 
-# manifest_template = '''
-# <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-# <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-# <assemblyIdentity
-#     version="5.0.0.0"
-#     processorArchitecture="x86"
-#     name="%(prog)s"
-#     type="win32"
-# />
-# <description>%(prog)s Program</description>
-# <dependency>
-#     <dependentAssembly>
-#         <assemblyIdentity
-#             type="win32"
-#             name="Microsoft.Windows.Common-Controls"
-#             version="6.0.0.0"
-#             processorArchitecture="X86"
-#             publicKeyToken="6595b64144ccf1df"
-#             language="*"
-#         />
-#     </dependentAssembly>
-# </dependency>
-# </assembly>
-# '''
-# 
-# RT_MANIFEST = 24
+def get_locales_target_and_source():
+	if RUNS_ON_WINDOWS:
+		return [
+			(
+				os.path.join('locale', 'de', 'LC_MESSAGES'),
+				[
+					os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'gettext', 'opsiclientd_de.mo'))
+				]
+			),
+			(
+				os.path.join('locale', 'fr', 'LC_MESSAGES'),
+				[
+					os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'gettext', 'opsiclientd_fr.mo'))
+				]
+			),
+		]
+	else:
+		return [
+			(
+				os.path.join('/etc', 'opsi-client-agent', 'locale'),
+				[
+					os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'gettext', 'opsiclientd_de.mo')),
+					os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'gettext', 'opsiclientd_fr.mo'))
+				]
+			)
+		]
 
-notifier = Target(
-	name = "notifier",
-	description = "opsi notifier",
-	script = "windows\\helpers\\notifier\\notifier.py",
-	# other_resources = [(RT_MANIFEST, 1, manifest_template % dict(prog="notifier"))],
-	icon_resources = [(1, "windows\\opsi.ico")]
-)
-
-opsiclientd_rpc = Target(
-	name = "opsiclientd_rpc",
-	description = "opsi client daemon rpc tool",
-	script = "windows\\helpers\\opsiclientd_rpc\\opsiclientd_rpc.py",
-	icon_resources = [(1, "windows\\opsi.ico")]
-)
-
-action_processor_starter = Target(
-	name = "action_processor_starter",
-	description = "opsi action processor starter",
-	script = "windows\\helpers\\action_processor_starter\\action_processor_starter.py",
-	icon_resources = [(1, "windows\\opsi.ico")]
-)
-
-network_performance = Target(
-	name = "network_performance",
-	description = "network performance",
-	script = "tests\\network_performance.py",
-	icon_resources = [(1, "windows\\opsi.ico")]
-)
 
 data_files = [
-	('Microsoft.VC90.MFC', glob.glob('Microsoft.VC90.MFC\\*.*')),
-	('Microsoft.VC90.CRT', glob.glob('Microsoft.VC90.CRT\\*.*')),
-	('lib\\Microsoft.VC90.MFC', glob.glob('Microsoft.VC90.MFC\\*.*')),
-	('lib\\Microsoft.VC90.CRT', glob.glob('Microsoft.VC90.CRT\\*.*')),
-	('notifier',                      [	'windows\\helpers\\notifier\\event.ini',
-						'windows\\helpers\\notifier\\action.ini',
-						'windows\\helpers\\notifier\\userlogin.ini',
-						'windows\\helpers\\notifier\\wait_for_gui.ini',
-						'windows\\helpers\\notifier\\block_login.ini',
-						'windows\\helpers\\notifier\\popup.ini',
-						'windows\\helpers\\notifier\\shutdown.ini',
-						'windows\\helpers\\notifier\\event.bmp',
-						'windows\\helpers\\notifier\\action.bmp',
-						'windows\\helpers\\notifier\\userlogin.bmp',
-						'windows\\helpers\\notifier\\wait_for_gui.bmp',
-						'windows\\helpers\\notifier\\block_login.bmp',
-						'windows\\helpers\\notifier\\popup.bmp',
-						'windows\\opsi.ico' ]),
-	('opsiclientd',                   [	'windows\\opsiclientd.conf']),
-	('locale\\de\\LC_MESSAGES',       [     '..\\gettext\\opsiclientd_de.mo']),
-	('locale\\fr\\LC_MESSAGES',       [     '..\\gettext\\opsiclientd_fr.mo']),
-	('opsiclientd\\extend.d', glob.glob('..\\extend.d\*.*')),
+	(os.path.join('opsiclientd', 'extend.d'), glob.glob(os.path.join('..', 'extend.d', '*.*'))),
 ]
-data_files += tree('opsiclientd\\static_html', '..\\static_html')
+data_files += get_locales_target_and_source()
 
-setup(
-	options = {
+if RUNS_ON_WINDOWS:
+	data_files += tree(os.path.join('opsiclientd', 'static_html'), os.path.join('..', 'static_html'))
+	data_files += [
+		('Microsoft.VC90.MFC', glob.glob('Microsoft.VC90.MFC\\*.*')),
+		('Microsoft.VC90.CRT', glob.glob('Microsoft.VC90.CRT\\*.*')),
+		('lib\\Microsoft.VC90.MFC', glob.glob('Microsoft.VC90.MFC\\*.*')),
+		('lib\\Microsoft.VC90.CRT', glob.glob('Microsoft.VC90.CRT\\*.*')),
+		('notifier', [
+			'windows\\helpers\\notifier\\event.ini',
+			'windows\\helpers\\notifier\\action.ini',
+			'windows\\helpers\\notifier\\userlogin.ini',
+			'windows\\helpers\\notifier\\wait_for_gui.ini',
+			'windows\\helpers\\notifier\\block_login.ini',
+			'windows\\helpers\\notifier\\popup.ini',
+			'windows\\helpers\\notifier\\shutdown.ini',
+			'windows\\helpers\\notifier\\event.bmp',
+			'windows\\helpers\\notifier\\action.bmp',
+			'windows\\helpers\\notifier\\userlogin.bmp',
+			'windows\\helpers\\notifier\\wait_for_gui.bmp',
+			'windows\\helpers\\notifier\\block_login.bmp',
+			'windows\\helpers\\notifier\\popup.bmp',
+			'windows\\opsi.ico'
+			]),
+		('opsiclientd', [os.path.join('windows', 'opsiclientd.conf')]),
+	]
+else:
+	data_files += [
+		(
+			os.path.join('/etc', 'opsi-client-agent', 'opsiclientd'),
+			[os.path.join('linux', 'opsiclientd.conf')]
+		)
+	]
+	data_files += tree(os.path.join('/etc', 'opsi-client-agent', 'opsiclientd', 'static_html'), os.path.join('..', 'static_html'))
+
+# TODO: better Version settings!
+opsiClientDeamonVersion = '0.1a'
+
+setup_options = {
+	"data_files": data_files,
+	"name": "opsiclientd",
+	"version": opsiClientDeamonVersion,
+    "url": 'http://www.opsi.org/',
+}
+
+if RUNS_ON_WINDOWS:
+	opsiclientd = Target(
+		name = "opsiclientd",
+		description = opsiclientdDescription,
+		script = "opsiclientd.py",
+		modules = ["opsiclientd"],
+		icon_resources = [(1, "windows\\opsi.ico")]
+	)
+
+	notifier = Target(
+		name = "notifier",
+		description = "opsi notifier",
+		script = "windows\\helpers\\notifier\\notifier.py",
+		icon_resources = [(1, "windows\\opsi.ico")]
+	)
+
+	opsiclientd_rpc = Target(
+		name = "opsiclientd_rpc",
+		description = "opsi client daemon rpc tool",
+		script = "windows\\helpers\\opsiclientd_rpc\\opsiclientd_rpc.py",
+		icon_resources = [(1, "windows\\opsi.ico")]
+	)
+
+	action_processor_starter = Target(
+		name = "action_processor_starter",
+		description = "opsi action processor starter",
+		script = "windows\\helpers\\action_processor_starter\\action_processor_starter.py",
+		icon_resources = [(1, "windows\\opsi.ico")]
+	)
+
+	network_performance = Target(
+		name = "network_performance",
+		description = "network performance",
+		script = "tests\\network_performance.py",
+		icon_resources = [(1, "windows\\opsi.ico")]
+	)
+
+	# These are options required by py2exe
+	setup_options['options'] = {
 		"py2exe": {
 			"compressed": 1,
-			#"bundle_files": 1,
 			"optimize": 2,
 			"excludes": excludes,
 			"packages": packages
 		}
-	},
-	data_files = data_files,
-	zipfile = "lib/library.zip",
-	service = [ opsiclientd ],
-	console = [ network_performance ],
-	windows = [ notifier, opsiclientd_rpc, action_processor_starter ],
-)
-for lang in os.listdir(os.path.join("dist", "locale")):
-	dn = os.path.join("dist", "locale", lang, "LC_MESSAGES")
-	for mo in os.listdir(dn):
-		src = os.path.join(dn, mo)
-		if mo.endswith('_%s.mo' % lang):
-			dst = os.path.join(dn, mo.split('_%s.mo' % lang)[0] + '.mo')
-			os.rename(src, dst)
+	}
 
-os.unlink(os.path.join("dist", "w9xpopen.exe"))
+	setup_options['zipfile'] = "lib/library.zip"
+	setup_options['service'] = [opsiclientd]
+	setup_options['console'] = [network_performance]
+	setup_options['windows'] = [notifier, opsiclientd_rpc, 	action_processor_starter]
+else:
+	setup_options['scripts'] = ['opsiclientd.py']
 
-print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-print "!!!   On the target machine always replace exe AND lib   !!!"
-print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+setup(**setup_options)
 
+locale_path = os.path.join("dist", "locale")
+if os.path.exists(locale_path):
+	for lang in os.listdir(locale_path):
+		dn = os.path.join("dist", "locale", lang, "LC_MESSAGES")
+		for mo in os.listdir(dn):
+			src = os.path.join(dn, mo)
+			if mo.endswith('_%s.mo' % lang):
+				dst = os.path.join(dn, mo.split('_%s.mo' % lang)[0] + '.mo')
+				os.rename(src, dst)
+
+if RUNS_ON_WINDOWS:
+	os.unlink(os.path.join("dist", "w9xpopen.exe"))
+
+	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	print "!!!   On the target machine always replace exe AND lib   !!!"
+	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
