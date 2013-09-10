6a230dd7c71b0415d2e397d47b7d2b5c7ca99c38
Niko Wenselowski <n.wenselowski@uib.de>
2013-09-10 09:38:30 +0000

    Imports, pep8.

--- src/ocdlib/__init__.py
+++ src/ocdlib/__init__.py
@@ -1,69 +1,66 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-   = = = = = = = = = = = = = = = = = = =
-   =   Dummy                           =
-   = = = = = = = = = = = = = = = = = = =
-   
-   This module is part of the desktop management solution opsi
-   (open pc server integration) http://www.opsi.org
-   
-   Copyright (C) 2010 uib GmbH
-   
-   http://www.uib.de/
-   
-   All rights reserved.
-   
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 2 as
-   published by the Free Software Foundation.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   
-   @copyright:	uib GmbH <info@uib.de>
-   @author: Erol Ueluekmen <e.ueluekmen@uib.de>
-   @license: GNU General Public License version 2
+ocdlib
+
+This module is part of the desktop management solution opsi
+(open pc server integration) http://www.opsi.org
+
+Copyright (C) 2010 uib GmbH
+
+http://www.uib.de/
+
+All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+@copyright:	uib GmbH <info@uib.de>
+@author: Erol Ueluekmen <e.ueluekmen@uib.de>
+@license: GNU General Public License version 2
 """
 
-# OPSI imports
-from OPSI.Logger import *
-from OPSI.Types import *
+import os
+
+from OPSI.Logger import Logger
+from OPSI.Types import forceUrl, forceUnicode, forceBool, forceHostId, forceProductIdList
 from OPSI import System
 
-# Get logger instance
 logger = Logger()
-
 
 
 def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True, masterOnly=False):
 	productIds = forceProductIdList(productIds)
-	
+
 	logger.notice(u"Selecting depot for products %s" % productIds)
-	
+
 	if event and event.eventConfig.useCachedProducts:
 		cacheDepotDir = os.path.join(config.get('cache_service', 'storage_dir'), 'depot').replace('\\', '/').replace('//', '/')
 		logger.notice(u"Using depot cache: %s" % cacheDepotDir)
 		config.setTemporaryDepotDrive(cacheDepotDir.split(':')[0] + u':')
 		config.set('depot_server', 'url', 'smb://localhost/noshare/' + ('/'.join(cacheDepotDir.split('/')[1:])))
 		return
-	
+
 	if not configService:
 		raise Exception(u"Not connected to config service")
-	
+
 	if configService.isLegacyOpsi():
 		return
-	
+
 	selectedDepot = None
-	
+
 	configService.backend_setOptions({"addConfigStateDefaults": True})
-	
+
 	depotIds = []
 	dynamicDepot = False
 	depotProtocol = 'cifs'
@@ -92,7 +89,7 @@ def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True
 			dynamicDepot = forceBool(configState.values[0])
 		elif (configState.configId == 'clientconfig.depot.protocol') and configState.values and configState.values[0] and (configState.values[0] == 'webdav'):
 			depotProtocol = 'webdav'
-	
+
 	if dynamicDepot:
 		if not depotIds:
 			logger.info(u"Dynamic depot selection enabled")
@@ -100,7 +97,7 @@ def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True
 			logger.info(u"Dynamic depot selection enabled, but depot is already selected")
 	else:
 		logger.info(u"Dynamic depot selection disabled")
-	
+
 	if not depotIds:
 		clientToDepotservers = configService.configState_getClientToDepotserver(
 				clientIds  = [ config.get('global', 'host_id') ],
@@ -108,11 +105,11 @@ def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True
 				productIds = productIds)
 		if not clientToDepotservers:
 			raise Exception(u"Failed to get depot config from service")
-		
+
 		depotIds = [ clientToDepotservers[0]['depotId'] ]
 		if dynamicDepot:
 			depotIds.extend(clientToDepotservers[0].get('alternativeDepotIds', []))
-		
+
 	masterDepot = None
 	alternativeDepots = []
 	for depot in configService.host_getObjects(type = 'OpsiDepotserver', id = depotIds):
@@ -122,7 +119,7 @@ def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True
 			alternativeDepots.append(depot)
 	if not masterDepot:
 		raise Exception(u"Failed to get info for master depot '%s'" % depotIds[0])
-	
+
 	logger.info(u"Master depot for products %s is %s" % (productIds, masterDepot.id))
 	selectedDepot = masterDepot
 	if dynamicDepot:
@@ -130,9 +127,8 @@ def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True
 			logger.info(u"Got alternative depots for products: %s" % productIds)
 			for i in range(len(alternativeDepots)):
 				logger.info(u"%d. alternative depot is %s" % ((i+1), alternativeDepots[i].id))
-			
+
 			try:
-					
 				defaultInterface = None
 				networkInterfaces = System.getNetworkInterfaces()
 				if not networkInterfaces:
@@ -153,13 +149,13 @@ def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True
 					"netmask":        forceUnicode(defaultInterface.ipAddressList.ipMask),
 					"defaultGateway": forceUnicode(defaultInterface.gatewayList.ipAddress)
 				}
-				
+
 				logger.info(u"Passing client configuration to depot selection algorithm: %s" % clientConfig)
-				
+
 				depotSelectionAlgorithm = configService.getDepotSelectionAlgorithm()
 				logger.debug2(u"depotSelectionAlgorithm:\n%s" % depotSelectionAlgorithm)
 				exec(depotSelectionAlgorithm)
-				selectedDepot = selectDepot(clientConfig = clientConfig, masterDepot = masterDepot, alternativeDepots = alternativeDepots)
+				selectedDepot = selectDepot(clientConfig=clientConfig, masterDepot=masterDepot, alternativeDepots=alternativeDepots)
 				if not selectedDepot:
 					selectedDepot = masterDepot
 			except Exception, e:
@@ -173,6 +169,3 @@ def selectDepotserver(config, configService, event, productIds=[], cifsOnly=True
 		config.set('depot_server', 'url', selectedDepot.depotWebdavUrl)
 	else:
 		config.set('depot_server', 'url', selectedDepot.depotRemoteUrl)
-
-
-
