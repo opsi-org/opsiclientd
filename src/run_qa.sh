ce626450b96009852d01bc4dedc2a5a6c43477e2
Niko Wenselowski <n.wenselowski@uib.de>
2014-05-13 14:46:53 +0000

    Executable.

--- src/run_qa.sh
+++ src/run_qa.sh
@@ -3,5 +3,5 @@
 TARGETS="ocdlib/ ocdlibnonfree/ scripts/opsiclientd windows/helpers/opsiclientd_rpc/opsiclientd_rpc.py windows/helpers/action_processor_starter/action_processor_starter.py"
 
 pylint --rcfile=../pylintrc $TARGETS > pylint.txt || echo 'pylint did not finish with return code 0'
-flake8 --ignore=W191 $TARGETS > pep8.txt || echo 'pep8 did not finish with return code 0'
+flake8 --exit-zero --ignore=W191 $TARGETS > pep8.txt
 nosetests --with-xunit --with-xcoverage --cover-package=ocdlib --cover-package=ocdlibnonfree tests/ || echo 'nosetests did not finish with return code 0'
