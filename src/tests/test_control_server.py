0ec16d3285741ae703b9ade3c3d23b9a39f06ac4
Niko Wenselowski <n.wenselowski@uib.de>
2014-05-26 14:08:56 +0000

    Testing that gui_startup is only there on Windows.

--- src/tests/test_control_server.py
+++ src/tests/test_control_server.py
@@ -8,6 +8,7 @@ import unittest
 from ocdlib.Config import ConfigImplementation
 import ocdlib.Events as Events
 import ocdlib.ControlServer as OCS
+import ocdlib.SystemCheck as sc
 
 
 # TODO: test ControlServer fire event
@@ -19,7 +20,12 @@ class ControlServerFiringEventTestCase(unittest.TestCase):
 		self.configPatcher = mock.patch('ocdlib.Events.config', self.temporaryConfig)
 		self.configPatcher.start()
 
-		self.defaultConfigFile = os.path.join(os.path.dirname(__file__), '..', 'windows', 'opsiclientd.conf')
+		if os.name == 'nt':
+			self.defaultConfigFile = os.path.join(os.path.dirname(__file__), '..', 'windows', 'opsiclientd.conf')
+		else:
+			self.defaultConfigFile = os.path.join(os.path.dirname(__file__), '..', 'linux', 'opsiclientd.conf')
+
+		self.temporaryConfig.set('global', 'config_file', self.defaultConfigFile)
 
 	def tearDown(self):
 		self.configPatcher.stop()
@@ -27,7 +33,6 @@ class ControlServerFiringEventTestCase(unittest.TestCase):
 		del self.defaultConfigFile
 
 	def testFiringEvent(self):
-		self.temporaryConfig.set('global', 'config_file', self.defaultConfigFile)
 		self.temporaryConfig.readConfigFile()
 
 		Events.createEventGenerators()
@@ -37,11 +42,24 @@ class ControlServerFiringEventTestCase(unittest.TestCase):
 		controlServer.fireEvent('on_demand')
 
 	def testFiringUnknownEventRaisesError(self):
-		self.temporaryConfig.set('global', 'config_file', self.defaultConfigFile)
 		self.temporaryConfig.readConfigFile()
 
 		controlServer = OCS.OpsiclientdRpcInterface(None)
 		self.assertRaises(ValueError, controlServer.fireEvent, 'foobar')
+
+
+	def testGUIStartupEventOnlyOnWindows(self):
+		self.temporaryConfig.readConfigFile()
+
+		Events.createEventGenerators()
+		configs = Events.getEventConfigs()
+
+		self.assertTrue(configs)
+		expected = False
+		if sc.RUNNING_ON_WINDOWS:
+			expected = True
+
+		self.assertEquals(expected, 'gui_startup' in configs)
 
 
 if __name__ == '__main__':
