04fb32b9d4e41dac4ff7f9b1bb26c4446e2a67b0
Niko Wenselowski <n.wenselowski@uib.de>
2014-08-08 13:30:49 +0000

    Simle output.

--- service_setup.sh
+++ service_setup.sh
@@ -1,21 +1,15 @@
 #! /bin/bash
 
-echo .
-echo   Aktueller PC: $HOSTNAME
-echo   You are at PC: $HOSTNAME
-echo .
-echo   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-echo   !!                                                                    !!
-echo   !!           Zum Starten der opsiclientd Installation                 !!
-echo   !!             druecken Sie bitte eine beliebige Taste                !!
-echo   !!        Zum Abbrechen schliessen Sie einfach dieses Fenster         !!
-echo   !!                                                                    !!
-echo   !!          To start the installation of the opsiclientd              !!
-echo   !!                        just press any key                          !!
-echo   !!                 To cancel just close this window                   !!
-echo   !!                                                                    !!
-echo   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-echo .
+echo "Aktueller PC: $HOSTNAME"
+echo "Current PC: $HOSTNAME"
+echo ""
+echo "Zum Starten der opsiclientd Installation"
+echo "druecken Sie bitte eine beliebige Taste"
+echo "Zum Abbrechen schliessen Sie einfach dieses Fenster"
+echo ""
+echo "To start the installation of the opsiclientd"
+echo "just press any key"
+echo "To cancel just close this window"
 read
 
 mkdir -p /tmp/opsiclientd
@@ -24,7 +18,7 @@ cp -r . /tmp/opsiclientd
 chmod u+x /tmp/opsiclientd/opsi-script/32/opsi-script-nogui
 /tmp/opsiclientd/opsi-script/32/opsi-script-nogui -batch /tmp/opsiclientd/setup.opsiscript /var/log/opsi-client-agent/opsi-script/opsiclientd.log
 
-echo .
-echo   Installation abgeschlossen
-echo   Installation completed
-echo .
+echo ""
+echo "Installation abgeschlossen"
+echo "Installation completed"
+echo ""
