1d67e73e602d5842ec184520fe2e9e6673ae6968
Erol Ülükmen <e.ueluekmen@uib.de>
2015-04-13 10:39:37 +0000

    shutdown_starter added to openSource part from opsiclientd and integrated in build-process

--- src/windows/helpers/opsiclientd_shutdown_starter/opsiclientd_shutdown_starter.py
+++ src/windows/helpers/opsiclientd_shutdown_starter/opsiclientd_shutdown_starter.py
@@ -34,16 +34,13 @@
 __version__ = '4.0.5'
 
 # Imports
-import sys, os, locale, time, base64, codecs
+import sys, os, time
 
 from OPSI import System
 from OPSI.Logger import *
 from OPSI.Backend.JSONRPC import JSONRPCBackend
 
-from OPSI.Util import md5sum
 from OPSI.Types import forceBool
-from hashlib import md5
-from twisted.conch.ssh import keys
 
 mydebug = 1
 
@@ -73,7 +70,6 @@ try:
 			if username and password:
 				break
 	
-
 	# Connect local service
 	be = JSONRPCBackend(username = username, password = password, address = u'https://localhost:4441/opsiclientd')
 	if mydebug :
@@ -84,58 +80,6 @@ try:
 			print u"State installation pending detected, don't starting shutdown event."
 		os.exit(0)
 		
-	# Check modules file
-	try:
-		modules = None
-		helpermodules = {}
-		## backendinfo = self._configService.backend_info()
-		backendinfo = be.getBackendInfo()
-		modules = backendinfo['modules']
-		helpermodules = backendinfo['realmodules']
-	
-		if not modules.get('install_by_shutdown'):
-			raise Exception(u"install_by_shutdown not available - module currently disabled")
-		if not modules.get('customer'):
-			raise Exception(u"install_by_shutdown not available - no customer set in modules file")
-		if not modules.get('valid'):
-			raise Exception(u"install_by_shutdown not available - modules file invalid")
-		if (modules.get('expires', '') != 'never') and (time.mktime(time.strptime(modules.get('expires', '2000-01-01'), "%Y-%m-%d")) - time.time() <= 0):
-			raise Exception(u"install_by_shutdown not available - modules file expired")
-
-		if mydebug :
-			print u"modules: passed first checkpoint."
-
-		# logger.info(u"Verifying modules file signature")
-		publicKey = keys.Key.fromString(data = base64.decodestring('AAAAB3NzaC1yc2EAAAADAQABAAABAQCAD/I79Jd0eKwwfuVwh5B2z+S8aV0C5suItJa18RrYip+d4P0ogzqoCfOoVWtDojY96FDYv+2d73LsoOckHCnuh55GA0mtuVMWdXNZIE8Avt/RzbEoYGo/H0weuga7I8PuQNC/nyS8w3W8TH4pt+ZCjZZoX8S+IizWCYwfqYoYTMLgB0i+6TCAfJj3mNgCrDZkQ24+rOFS4a8RrjamEz/b81noWl9IntllK1hySkR+LbulfTGALHgHkDUlk0OSu+zBPw/hcDSOMiDQvvHfmR4quGyLPbQ2FOVm1TzE0bQPR+Bhx4V8Eo2kNYstG2eJELrz7J1TJI0rCjpB+FQjYPsP')).keyObject
-		data = u''
-		mks = modules.keys()
-		mks.sort()
-		for module in mks:
-			if module in ('valid', 'signature'):
-				continue
-			if helpermodules.has_key(module):
-				val = helpermodules[module]
-				if int(val) > 0:
-					modules[module] = True
-			else:
-				val = modules[module]
-				if (val == False): val = 'no'
-				if (val == True):  val = 'yes'
-			data += u'%s = %s\r\n' % (module.lower().strip(), val)
-		if not bool(publicKey.verify(md5(data).digest(), [ long(modules['signature']) ])):
-			raise Exception(u"install_by_shutdown not available - modules file invalid")
-		## logger.notice(u"Modules file signature verified (customer: %s)" % modules.get('customer'))
-		if mydebug :
-			print u"Modules file signature verified (customer: %s)" % modules.get('customer')
-
-	except Exception, e:
-		## self.disconnectConfigService()
-		raise
-
-	if mydebug :
-		print u"Check completed."
-
-
 	# Trying to fire myEvent
 	be.fireEvent(myEvent)
 	if mydebug :
