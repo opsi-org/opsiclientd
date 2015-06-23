10f4c65e0fd4ea77179e97db253604ee6ba88d19
Niko Wenselowski <n.wenselowski@uib.de>
2015-06-23 10:19:43 +0200

    Setting -e in the shebang line.

--- build_binary.sh
+++ build_binary.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -e
 
 # This file is part of opsiclientd.
 # Copyright (C) 2013 uib GmbH <info@uib.de>
@@ -15,9 +15,6 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Every error leads to a fail
-set -e
 
 cd src/
 python setup-cx-freeze.py build
