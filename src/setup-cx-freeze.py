b6f30e2f63698f4559b275e96091969bb3e184ab
Niko Wenselowski <n.wenselowski@uib.de>
2015-07-02 14:09:04 +0200

    We do not include the librsync-files anymore.
    During the installation we install librsync1 on the client and this should reduce the problems with cx_freeze not properly including the lib.

--- src/setup-cx-freeze.py
+++ src/setup-cx-freeze.py
@@ -61,34 +61,6 @@ buildOptions = {
     ]
 }
 
-localDirectory = os.path.dirname(__file__)
-
-opsiClientDeamonVersion = None
-fileWithVersion = os.path.join(localDirectory, 'ocdlib', '__init__.py')
-with open(fileWithVersion, 'r') as f:
-    for line in f:
-        if "__version__" in line:
-            opsiClientDeamonVersion = line.split('=', 1)[1].strip()[1:-1]
-            break
-
-if not opsiClientDeamonVersion:
-    raise Exception("Failed to find version.")
-
-
-if os.path.exists("/usr/lib/x86_64-linux-gnu/librsync.so.1"):
-    buildOptions['include_files'].append(("/usr/lib/x86_64-linux-gnu/librsync.so.1", "librsync.so.1"))
-elif os.path.exists("/usr/lib/librsync.so.1"):
-    buildOptions['include_files'].append(("/usr/lib/librsync.so.1", "librsync.so.1"))
-elif os.path.exists("/usr/lib64/librsync.so.1"):
-    buildOptions['include_files'].append(("/usr/lib64/librsync.so.1", "librsync.so.1"))
-else:
-    print("Warning: could not find librsync.so.1!")
-
-if os.path.exists('/usr/lib64/librsync.so.1.0.2'):
-    buildOptions['include_files'].append(("/usr/lib64/librsync.so.1.0.2", "librsync.so.1.0.2"))
-elif os.path.exists('/usr/lib/librsync.so.1.0.2'):
-    buildOptions['include_files'].append(("/usr/lib/librsync.so.1.0.2", "librsync.so.1.0.2"))
-
 buildFreeVersion = False
 if '--free' in sys.argv:
     buildFreeVersion = True
@@ -104,6 +76,8 @@ else:
 opsiClientDeamonVersion = getVersion()
 print("Building {0} (Version {1})".format(opsiclientdDescription,
                                           opsiClientDeamonVersion))
+
+localDirectory = os.path.dirname(__file__)
 
 executables = [
     Executable(os.path.join(localDirectory, 'scripts', 'opsiclientd'), 'Console', targetName='opsiclientd')
