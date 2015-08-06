f7fc2a070e9bd7f4e5d53a8303c65dd5220ad453
Niko Wenselowski <n.wenselowski@uib.de>
2015-08-06 18:27:26 +0200

    First shot for a test of the new functions.

---
+++ src/ocdlibnonfree/tests/test_opsiclientd.py
@@ -0,0 +1,51 @@
+
+import unittest
+
+from ocdlibnonfree.Posix import Opsiclientd
+
+import os
+import shutil
+import tempfile
+from contextlib import contextmanager
+from functools import wraps
+
+
+@contextmanager
+def workInTemporaryDirectory(tempDir=None):
+    """
+    Creates a temporary folder to work in. Deletes the folder afterwards.
+
+    :param tempDir: use the given dir as temporary directory. Will not \
+be deleted if given.
+    """
+    temporary_folder = tempDir or tempfile.mkdtemp()
+    with cd(temporary_folder):
+        yield temporary_folder
+
+    if not tempDir and os.path.exists(temporary_folder):
+        shutil.rmtree(temporary_folder)
+
+
+@contextmanager
+def cd(path):
+    old_dir = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_dir)
+
+
+class OpsiclientdRebootCoordinationTestCase(unittest.TestCase):
+    def test_requesting_reboot(self):
+        with workInTemporaryDirectory() as tempDir:
+            with mock.patch('ocdlibnonfree.Posix.Opsiclientd._PID_DIR', tempDir):
+                ocd = Opsiclientd()
+
+                self.assertFalse(ocd.isRebootRequested())
+
+                rebootFile = os.path.join(tempDir, 'reboot')
+                with open(rebootFile, 'w'):
+                    pass
+
+                ocd.clearRebootRequest()
+                self.assertFalse(ocd.isRebootRequested())
+
