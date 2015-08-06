e33279535ae6aef13f786467983012e0371db11d
Niko Wenselowski <n.wenselowski@uib.de>
2015-08-06 17:51:05 +0200

    First shot of reboot mechanism.
    Currently untested.
    
    References #1492

---
+++ src/ocdlibnonfree/Posix.py
@@ -0,0 +1,64 @@
+#! python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010-2015 uib GmbH
+# http://www.uib.de/
+# All rights reserved.
+"""
+Non-free Posix part of opsiclientd
+
+:copyright: uib GmbH <info@uib.de>
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+"""
+
+import os
+import os.path
+import sys
+import time
+
+import OPSI.System as System
+from OPSI.Logger import Logger, LOG_NONE, LOG_NOTICE
+from OPSI.Types import forceUnicode
+
+from ocdlib.Opsiclientd import Opsiclientd
+
+logger = Logger()
+
+
+class OpsiclientdPosix(Opsiclientd):
+
+    _PID_DIR = os.path.join("/var/", "run", "opsiclientd")
+
+    # TODO: beim init das Verzeichnis erstellen
+
+    def clearRebootRequest(self):
+        rebootFile = os.path.join(self._PID_DIR, "reboot")
+        try:
+            os.remove(rebootFile)
+        except OSError as err:
+            logger.debug("Failed to remove reboot file {1!r}: {0}".format(err, rebootFile))
+
+    def clearShutdownRequest(self):
+        shutdownFile = os.path.join(self._PID_DIR, "shutdown")
+        try:
+            os.remove(shutdownFile)
+        except OSError as err:
+            logger.debug("Failed to remove shutdwn file {1!r}: {0}".format(err, shutdownFile))
+
+    def isRebootRequested(self):
+        rebootFile = os.path.join(self._PID_DIR, "reboot")
+        return os.path.exists(rebootFile)
+
+    def isShutdownRequested(self):
+        shutdownFile = os.path.join(self._PID_DIR, "shutdown")
+        return os.path.exists(shutdownFile)
+
+    def rebootMachine(self):
+        self._isRebootTriggered = True
+        self.clearRebootRequest()
+        System.reboot(wait=3)
+
+    def shutdownMachine(self):
+        self._isShutdownTriggered = True
+        self.clearShutdownRequest()
+        System.shutdown(wait=3)
