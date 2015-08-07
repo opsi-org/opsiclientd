4bfbfabcf5b5fe2537756cf17daa2de80a880467
Niko Wenselowski <n.wenselowski@uib.de>
2015-08-07 17:22:04 +0200

    Using py.test for tests and pytest-cov for coverage.

--- src/run_qa.sh
+++ src/run_qa.sh
@@ -4,4 +4,4 @@ TARGETS="ocdlib/ ocdlibnonfree/ scripts/opsiclientd windows/helpers/opsiclientd_
 
 pylint --rcfile=../pylintrc $TARGETS > pylint.txt || echo 'pylint did not finish with return code 0'
 flake8 --exit-zero --ignore=W191 $TARGETS > pep8.txt
-nosetests --with-xunit --with-xcoverage --cover-package=ocdlib --cover-package=ocdlibnonfree tests/ || echo 'nosetests did not finish with return code 0'
+py.test --junitxml=testreport.xml --cov ocdlib --cov ocdlibnonfree --cov-report xml --quiet tests/
