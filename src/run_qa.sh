78a9e0d27958c73ff9c6b26e3e0af2efd4abf68a
Niko Wenselowski <n.wenselowski@uib.de>
2015-08-26 10:17:18 +0200

    Adding scripts for QA.

---
+++ src/run_qa.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+TARGETS="ocdlib/ ocdlibnonfree/ scripts/opsiclientd windows/helpers/opsiclientd_rpc/opsiclientd_rpc.py windows/helpers/action_processor_starter/action_processor_starter.py linux/notifier.py linux/opsiclientd_rpc.py"
+
+pylint --rcfile=../pylintrc $TARGETS > pylint.txt || echo 'pylint did not finish with return code 0'
+flake8 --exit-zero --ignore=W191 $TARGETS > pep8.txt
+py.test --junitxml=testreport.xml --cov ocdlib --cov ocdlibnonfree --cov-report xml --quiet tests/
