029bf0586c71bc2956b9eedff93e664cdae397f9
Niko Wenselowski <n.wenselowski@uib.de>
2015-09-28 15:52:53 +0200

    Using str.format powers \o/

--- src/ocdlib/State.py
+++ src/ocdlib/State.py
@@ -98,7 +98,7 @@ class StateImplementation(object):
 		elif name in self._state:
 			return self._state[name]
 
-		logger.warning(u"Unknown state name '{0}', returning {1}".format(name, default))
+		logger.warning(u"Unknown state name {0!r}, returning {1}".format(name, default))
 		return default
 
 	def set(self, name, value):
