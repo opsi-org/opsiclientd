4b9ce89c3968dae3570b7de94bb1f283d30979fc
Erol Ueluekmen <e.ueluekmen@uib.de>
2016-06-23 01:13:35 +0200

    working

--- src/ocdlib/SoftwareOnDemand.py
+++ src/ocdlib/SoftwareOnDemand.py
@@ -504,7 +504,7 @@ class WorkerKioskJsonRpc(WorkerOpsiJsonRpc, ServiceConnection):
 		ServiceConnection.__init__(self)
 
 	def _getAllowedMethods(self):
-		return = [
+	    return = [
             "getPossibleMethods_listOfHashes",
             "backend_getInterface",
 			"getGeneralConfigValue",
