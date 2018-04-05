cb77147e720e2e62ff09fe2093c87d5b61f3e12d
Niko Wenselowski <n.wenselowski@uib.de>
2018-04-05 14:15:55 +0200

    Add tests for control server and event firing.

---
+++ src/tests/test_control_server.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+import ocdlib.Events as Events
+import ocdlib.ControlServer as OCS
+
+import pytest
+
+
+@pytest.fixture
+def preparedConfig(config, configFile):
+	config.set('global', 'config_file', configFile)
+	yield
+
+
+def testFiringEvent(preparedConfig):
+	preparedConfig.readConfigFile()
+
+	Events.createEventGenerators()
+	Events.getEventConfigs()
+
+	controlServer = OCS.OpsiclientdRpcInterface(None)
+	controlServer.fireEvent('on_demand')
+
+
+def testFiringUnknownEventRaisesError(preparedConfig):
+	preparedConfig.readConfigFile()
+
+	controlServer = OCS.OpsiclientdRpcInterface(None)
+	with pytest.raises(ValueError):
+		controlServer.fireEvent('foobar')
+
+
+@pytest.mark.parametrize("on_windows", [os.name == 'nt'])
+def testGUIStartupEventOnlyOnWindows(preparedConfig, on_windows):
+	preparedConfig.readConfigFile()
+
+	Events.createEventGenerators()
+	configs = Events.getEventConfigs()
+
+	assert configs
+	assert on_windows == ('gui_startup' in configs)
