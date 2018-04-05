e22ccec487b11867f4cd10901df8913b69136c97
Niko Wenselowski <n.wenselowski@uib.de>
2018-04-05 10:57:55 +0200

    We are working on git.

--- create_archive.sh
+++ create_archive.sh
@@ -20,8 +20,8 @@ cp -r src/ocdlib src/windows src/setup.py ${tmpdir}/src
 find ${tmpdir} -iname "*.pyc"   -exec rm "{}" \;
 find ${tmpdir} -iname "*.marks" -exec rm "{}" \;
 find ${tmpdir} -iname "*~"      -exec rm "{}" \;
-find ${tmpdir} -iname "*.svn"   -exec rm -rf "{}" \; 2>/dev/null
-
+find ${tmpdir} -iname ".git"   -exec rm -rf "{}" \; 2>/dev/null
+find ${tmpdir} -iname ".gitignore"   -exec rm -rf "{}" \; 2>/dev/null
 
 cd ${tmpdir}/..
 tar cjvf ${destdir}/${packagename}-${version}.tar.bz2 ${packagename}-${version}
