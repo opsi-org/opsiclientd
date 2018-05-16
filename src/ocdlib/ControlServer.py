d43c60f1b1d8d527838de5f0b5a46a9407e3a6f1
Niko Wenselowski <n.wenselowski@uib.de>
2018-05-16 08:23:09 +0200

    Begin to overhaul imports.

--- src/ocdlib/ControlServer.py
+++ src/ocdlib/ControlServer.py
@@ -27,25 +27,25 @@ procedure calls
 :license: GNU Affero General Public License version 3
 """
 
-# Imports
-from OpenSSL import SSL
-import base64, urllib, codecs, time, re
-import win32security, win32net
+import codecs
+import time
+import re
+import win32security
+import win32net
 
 # Twisted imports
-from twisted.internet import defer, threads, reactor
-from OPSI.web2 import resource, stream, server, http, responsecode, static, http_headers
+from twisted.internet import reactor
+from OPSI.web2 import resource, stream, server, http, responsecode
 from OPSI.web2.channel.http import HTTPFactory
 
 # OPSI imports
 from OPSI.Logger import *
-from OPSI.Types import *
+from OPSI.Types import forceBool, forceInt, forceUnicode
 from OPSI.Util import *
 from OPSI import System
 from OPSI.Service import SSLContext, OpsiService
-from OPSI.Service.Worker import WorkerOpsi, WorkerOpsiJsonRpc, WorkerOpsiJsonInterface, WorkerOpsiDAV, interfacePage
+from OPSI.Service.Worker import WorkerOpsi, WorkerOpsiJsonRpc, WorkerOpsiJsonInterface
 from OPSI.Service.Resource import ResourceOpsi, ResourceOpsiJsonRpc, ResourceOpsiJsonInterface, ResourceOpsiDAV
-from OPSI.Backend.BackendManager import BackendManager
 
 from ocdlib.Exceptions import *
 from ocdlib.ControlPipe import OpsiclientdRpcPipeInterface
@@ -53,16 +53,12 @@ from ocdlib.Config import Config
 from ocdlib.Events import eventGenerators
 from ocdlib.Timeline import Timeline
 from ocdlib.OpsiService import ServiceConnection
-from ocdlib.SoftwareOnDemand import WorkerSoftwareOnDemand, ResourceSoftwareOnDemand, WorkerKioskJsonRpc, ResourceKioskJsonRpc
+from ocdlib.SoftwareOnDemand import ResourceKioskJsonRpc
 
 logger = Logger()
 config = Config()
 timeline = Timeline()
 
-try:
-	from ocdlibnonfree.CacheService import CacheService
-except Exception as e:
-	pass
 
 infoPage = u'''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
