729b397fdbc4352a8309d2c082394f54a7b51514
Niko Wenselowski <n.wenselowski@uib.de>
2019-05-09 15:19:44 +0200

    Import known classes.

--- src/ocdlibnonfree/CacheBackend.py
+++ src/ocdlibnonfree/CacheBackend.py
@@ -30,7 +30,8 @@ from OPSI.Backend.Replicator import BackendReplicator
 from OPSI.Exceptions import BackendConfigurationError, BackendUnaccomplishableError
 from OPSI.Logger import Logger
 from OPSI.Object import getIdentAttributes, objectsDiffer
-from OPSI.Object import *
+from OPSI.Object import LicenseOnClient, ProductOnClient
+from OPSI.Object import *  # required for dynamic class loading
 from OPSI.Types import forceHostId
 from OPSI.Util import blowfishDecrypt
 
