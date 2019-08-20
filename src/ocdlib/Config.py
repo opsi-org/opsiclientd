30fbbfc687c56bb4db348c44efc36dbf66163a66
Erol Ueluekmen <e.ueluekmen@uib.de>
2019-08-20 10:34:28 +0200

    Do not refuse empty proxy_url and working_window options.

--- src/ocdlib/Config.py
+++ src/ocdlib/Config.py
@@ -204,7 +204,11 @@ class ConfigImplementation(object):
 		logger.info(u"Setting config value %s.%s" % (section, option))
 		logger.debug(u"set(%s, %s, %s)" % (section, option, value))
 
-		if (option.find('command') == -1) and (option.find('productids') == -1) and (option.find('exclude_product_group_ids') == -1) and (option.find('include_product_group_ids') == -1) and (value == ''):
+		if 	(option.find('command') == -1) and (option.find('productids') == -1) and
+			(option.find('exclude_product_group_ids') == -1) and
+			(option.find('include_product_group_ids') == -1) and
+			(option.find('proxy_url') == -1) and
+			(option.find('working_window') == -1) and (value == ''):
 			logger.warning(u"Refusing to set empty value for config value '%s' of section '%s'" % (option, section))
 			return
 
