0c38abc44cca3353dabc9af3be1cbc8c1212d36f
Niko Wenselowski <n.wenselowski@uib.de>
2019-10-10 11:21:38 +0200

    Adding more tests.

---
+++ src/tests/test_event_config.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from ocdlib.EventConfiguration import EventConfig
+from ocdlib.Events import (DaemonShutdownEventConfig, DaemonStartupEventConfig,
+    PanicEventConfig, TimerEventConfig, ProcessActionRequestsEventConfig,
+    SwOnDemandEventConfig, SyncCompletedEventConfig)
+
+
+@pytest.fixture(params=[
+    DaemonShutdownEventConfig, DaemonStartupEventConfig, EventConfig,
+    PanicEventConfig, TimerEventConfig, ProcessActionRequestsEventConfig,
+    SwOnDemandEventConfig, SyncCompletedEventConfig
+])
+def configClass(request):
+    yield request.param
+
+
+def testCreatingNewEventConfig(configClass):
+    configClass("testevent")
+
+
+def testAttributesForWhiteAndBlackListExist(configClass):
+    config = configClass("testevent")
+
+    assert hasattr(config, 'excludeProductGroupIds')
+    assert hasattr(config, 'includeProductGroupIds')
