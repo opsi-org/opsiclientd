92c6587d9899ed042d172d3c3a233d4b848cf456
Niko Wenselowski <n.wenselowski@uib.de>
2019-10-11 13:19:01 +0200

    Forgot to merge.

--- src/ocdlib/EventProcessing.py
+++ src/ocdlib/EventProcessing.py
@@ -313,20 +313,12 @@ If this is `None` a random port will be chosen.
 			if not self.isConfigServiceConnected():
 				logger.warning(u"Cannot write log to service: not connected")
 				return
-<<<<<<< HEAD
-			self.setStatusMessage( _(u"Writing log to service") )
-			with codecs.open(config.get('global', 'log_file'), 'r', 'utf-8', 'replace') as f:
-				data = f.read()
-			data += u"-------------------- submitted part of log file ends here, see the rest of log file on client --------------------\n"
-
-=======
 			self.setStatusMessage(_(u"Writing log to service"))
 
 			with codecs.open(config.get('global', 'log_file'), 'r', 'utf-8', 'replace') as f:
 				data = f.read()
 
 			data += u"-------------------- submitted part of log file ends here, see the rest of log file on client --------------------\n"
->>>>>>> release/4.1.2
 			# Do not log jsonrpc request
 			logger.setFileLevel(LOG_WARNING)
 			self._configService.log_write('clientconnect', data.replace(u'\ufffd', u'?'), config.get('global', 'host_id'))
