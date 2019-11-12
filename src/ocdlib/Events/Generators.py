b99a71c1837da6940b5a03fb7129e31d50adefe2
Niko Wenselowski <der@nik0.de>
2019-11-12 15:36:42 +0100

    Better docstring.

--- src/ocdlib/Events/Generators.py
+++ src/ocdlib/Events/Generators.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Functions to (re)create and get event generators.
+Functions to create, reconfigure and get event generators.
 
 :copyright: uib GmbH <info@uib.de>
 :author: Jan Schneider <j.schneider@uib.de>
