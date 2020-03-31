6ec6743a908ba3e341b1a592690e0b1870ab7771
Erol Ueluekmen <e.ueluekmen@uib.de>
2020-03-31 22:35:58 +0200

    Working Window Fix

--- src/ocdlib/EventProcessing.py
+++ src/ocdlib/EventProcessing.py
@@ -1129,7 +1129,9 @@ class EventProcessingThread(KillableThread, ServiceConnection):
 						minute=int(e_minute),
 						second=0,
 						microsecond=0)
-			if end < start:
+			if now < start:
+				start = start - timedelta(days=1)
+			elif end < start:
 				end = end + timedelta(days=1)
 
 			if start < now < end:
