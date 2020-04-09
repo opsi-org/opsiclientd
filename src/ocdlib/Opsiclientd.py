ff450b5bb28ab38b2e009982f948aa7067800736
Erol Ueluekmen <e.ueluekmen@uib.de>
2020-04-09 23:41:38 +0200

    Version bumped

--- src/ocdlib/Opsiclientd.py
+++ src/ocdlib/Opsiclientd.py
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 @license: GNU Affero GPL version 3
 """
 
-__version__ = '4.0.100'
+__version__ = '4.0.101'
 
 # Imports
 import sys, os
