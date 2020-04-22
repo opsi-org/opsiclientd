896e70251e56a2639697e7113d37ec6d411abf1d
Jan Schneider <j.schneider@uib.de>
2020-04-22 14:07:03 +0200

    Add pyinstaller

---
+++ pyinstaller-build.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+import glob
+import codecs
+import shutil
+import platform
+import subprocess
+
+SCRIPTS = [
+	"opsiclientd"
+]
+HIDDEN_IMPORTS = [
+]
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+subprocess.check_call(["poetry", "install"])
+
+for d in ("dist", "build"):
+	if os.path.isdir(d):
+		shutil.rmtree(d)
+
+spec_a = ""
+spec_m = []
+spec_o = ""
+for script in SCRIPTS:
+	cmd = ["poetry", "run", "pyi-makespec"]
+	for hi in HIDDEN_IMPORTS:
+		cmd.extend(["--hidden-import", hi])
+	cmd.append(script)
+	subprocess.check_call(cmd)
+	with codecs.open("%s.spec" % script, "r", "utf-8") as f:
+		varname = script.replace("-", "_")
+		data = f.read()
+		data = re.sub(r"([\s\(])a\.", r"\g<1>" + varname + "_a.", data)
+		#print(data)
+		match = re.search(r"(.*)(a\s*=\s*)(Analysis[^\)]+\))(.*)", data, re.MULTILINE|re.DOTALL)
+		if not spec_a:
+			spec_a += match.group(1)
+		spec_a += "%s_a = %s\n" % (varname, match.group(3))
+		spec_o += match.group(4)
+		spec_m.append("(%s_a, '%s', '%s')" % (varname, script, script))
+
+with codecs.open("opsi-client-agent.spec", "w", "utf-8") as f:
+	f.write(spec_a)
+	f.write("\nMERGE( %s )\n" % ', '.join(spec_m))
+	f.write(spec_o)
+
+subprocess.check_call(["poetry", "run", "pyinstaller", "--log-level", "INFO", "opsi-client-agent.spec"])
+
+
+shutil.move("dist/%s" % SCRIPTS[0], "dist/opsi-client-agent")
+for script in SCRIPTS[1:]:
+	shutil.move("dist/%s/%s" % (script, script), "dist/opsi-client-agent/%s" % script)
+	shutil.rmtree("dist/%s" % script)
+
