82e00ddc6a274eb3d605f75e004cb2097fab2168
doerrer <n.doerrer@uib.de>
2020-10-16 08:34:04 +0200

    [chg] restructured bitlocker suspend error handling

--- opsiclientd/windows/opsiclientd.py
+++ opsiclientd/windows/opsiclientd.py
@@ -103,16 +103,17 @@ class OpsiclientdNT(Opsiclientd):
 				captureStderr=True,
 				waitForEnding=True
 			)
-			if "does not have an associated BitLocker volume" in result:
-				logger.warning("Attempting bitlocker suspend but can not find bitlocker volume")
-			else:
-				result = System.execute(
-					"powershell.exe -ExecutionPolicy Bypass -Command \"Get-BitLockerVolume | Suspend-BitLocker -RebootCount 1\"",
-					captureStderr=True,
-					waitForEnding=True
-				)
-			logger.info(result)
 		except Exception as e:
+			logger.warning("Attempting bitlocker suspend but can not find bitlocker volume")
+			return
+		try:
+			result = System.execute(
+				"powershell.exe -ExecutionPolicy Bypass -Command \"Get-BitLockerVolume | Suspend-BitLocker -RebootCount 1\"",
+				captureStderr=True,
+				waitForEnding=True
+			)
+			logger.info(result)
+		except Exception as e:		# on fail this will also prompt an error gui window
 			logger.error("Failed to suspend bitlocker: %s", e)
 	
 	def rebootMachine(self, waitSeconds=3):
