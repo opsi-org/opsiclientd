f549b14bf5d32a59b6590141f45c867a928dbfe8
doerrer <n.doerrer@uib.de>
2020-10-29 13:13:05 +0100

    [fix] removed redundant powershell command

--- opsiclientd/windows/opsiclientd.py
+++ opsiclientd/windows/opsiclientd.py
@@ -99,7 +99,7 @@ class OpsiclientdNT(Opsiclientd):
 				"powershell.exe -ExecutionPolicy Bypass -Command \""
 				"foreach($v in Get-BitLockerVolume){"
 				"if (-not ($v.EncryptionPercentage -eq '0'))"
-				"{Get-BitLockerVolume | Suspend-BitLocker -RebootCount 1; break}}\"",
+				"{$v | Suspend-BitLocker -RebootCount 1; break}}\"",
 				captureStderr=True,
 				waitForEnding=True,
 				timeout=20
