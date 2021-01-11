2cfd8399b3b7e1e4a190078898b0d2e4f314f325
Jan Schneider <j.schneider@uib.de>
2021-01-11 12:56:30 +0100

    [chg,new] New commandline interface for opsiclientd_rpc

--- opsiclientd/opsiclientdrpc.py
+++ opsiclientd/opsiclientdrpc.py
@@ -21,39 +21,133 @@
 :license: GNU Affero General Public License version 3
 """
 
+import codecs
 import os
 import sys
+import argparse
+import platform
 
-from opsicommon.logging import logger, init_logging, log_context, LOG_DEBUG
 from OPSI.Backend.JSONRPC import JSONRPCBackend
 # Do not remove this import, it's needed by using this module from CLI
 from OPSI import System # pylint: disable=unused-import
+from OPSI import __version__ as python_opsi_version
+
+from opsicommon.logging import (
+	logger, init_logging, log_context, secret_filter, LOG_DEBUG, LOG_NONE
+)
+from opsicommon.logging.constants import LOG_TRACE
 
 from opsiclientd import __version__, DEFAULT_FILE_LOG_FORMAT
 
+def get_opsi_host_key():
+	config_file = "/etc/opsi-client-agent/opsiclientd.conf"
+	if platform.system().lower() == 'windows':
+		config_file = os.path.join(
+			os.environ.get("PROGRAMFILES(X86)", os.environ.get("PROGRAMFILES")),
+			"opsi.org", "opsi-client-agent", "opsiclientd", "opsiclientd.conf"
+		)
+
+	with codecs.open(config_file, "r", "utf-8") as file:
+		for line in file.readlines():
+			line = line.strip()
+			if line.startswith("opsi_host_key"):
+				return line.split("=", 1)[-1].strip()
+
+	raise RuntimeError(f"Failed to find opsi_host_key in config file {config_file}")
+
+class ArgumentParser(argparse.ArgumentParser):
+	def error(self, message):
+		if len(sys.argv) in (5, 6):
+			raise DeprecationWarning("Legacy comandline arguments")
+		return argparse.ArgumentParser.error(self, message)
+
 def main():
 	with log_context({'instance' : os.path.basename(sys.argv[0])}):
-		if len(sys.argv) < 5:
-			print(f"Usage: {os.path.basename(sys.argv[0])} <username> <password> <port> [debug_logfile] <rpc>", file=sys.stderr)
-			sys.exit(1)
-
-		(username, password, port, rpc) = sys.argv[1:5] # pylint: disable=unbalanced-tuple-unpacking
-		if len(sys.argv) > 5:
-			rpc = sys.argv[5]
-			init_logging(
-				log_file=sys.argv[4],
-				file_level=LOG_DEBUG,
-				file_format=DEFAULT_FILE_LOG_FORMAT
+		parser = ArgumentParser()
+		parser.add_argument('--version',
+			action='version',
+			version=f"{__version__} [python-opsi={python_opsi_version}]"
+		)
+		parser.add_argument('--log-level',
+			default=LOG_NONE,
+			type=int,
+			choices=range(0, 10),
+			help=(
+				"Set the log level. "
+				"0: nothing, 1: essential, 2: critical, 3: errors, 4: warnings, 5: notices "
+				"6: infos, 7: debug messages, 8: trace messages, 9: secrets"
 			)
+		)
+		parser.add_argument('--log-file',
+			help="Set log file"
+		)
+		parser.add_argument('--address',
+			default="https://localhost:4441/opsiclientd",
+			help="Set service address"
+		)
+		parser.add_argument('--username',
+			help="Username to use for service connection."
+		)
+		parser.add_argument('--password',
+			help="Password to use for service connection (default: opsi host key)."
+		)
+		parser.add_argument('rpc',
+			help="The remote procedure call to execute."
+		)
 
-		logger.debug("argv: %s", sys.argv)
+		log_file = None
+		log_level = None
+		address = None
+		username = None
+		password = None
+		rpc = None
+		try:
+			args = parser.parse_args()
+			log_file = args.log_file
+			log_level = args.log_level
+			address = args.address
+			username = args.username
+			password = args.password
+			rpc = args.rpc
+			if not username and not password:
+				try:
+					password = get_opsi_host_key()
+					secret_filter.add_secrets([password])
+				except Exception as err: # pylint: disable=broad-except
+					raise RuntimeError(f"Failed to read opsi host key from config file: {err}")
 
-		address = f"https://localhost:{port}/opsiclientd"
+		except DeprecationWarning:
+			# Fallback to legacy comandline arguments
+			# <username> <password> <port> [debug-log-file] <rpc>
+			(username, password, port, rpc) = sys.argv[1:5] # pylint: disable=unbalanced-tuple-unpacking
+			secret_filter.add_secrets([password])
+			address = f"https://localhost:{port}/opsiclientd"
+			if len(sys.argv) > 5:
+				log_level = LOG_TRACE #LOG_DEBUG
+				log_file = sys.argv[4]
+				rpc = sys.argv[5]
+
+		init_logging(
+			log_file=log_file,
+			file_level=log_level,
+			stderr_level=LOG_NONE,
+			file_format=DEFAULT_FILE_LOG_FORMAT
+		)
+
+		logger.debug(
+			"log_file=%s, log_level=%s, address=%s, username=%s, password=%s, rpc=%s",
+			log_file, log_level, address, username, password, rpc
+		)
 
 		try:
-			with JSONRPCBackend(username=username, password=password, address=address) as backend: # pylint: disable=unused-variable
+			with JSONRPCBackend(
+				username=username,
+				password=password,
+				address=address
+			) as jsonrpc: # pylint: disable=unused-variable
 				logger.notice(f"Executing: {rpc}")
-				exec(f"backend.{rpc}") # pylint: disable=exec-used
+				result = eval(f"jsonrpc.{rpc}") # pylint: disable=exec-used
+				print(result)
 		except Exception as err: # pylint: disable=broad-except
 			logger.error(err, exc_info=True)
 			print(f"Error: {err}", file=sys.stderr)
