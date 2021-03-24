c222c1f7a176b2a4582f44d0b528ba8e8c15e826
Jan Schneider <j.schneider@uib.de>
2021-03-24 22:37:50 +0100

    [fix] Use .hostname instead of ._host

--- opsiclientd/nonfree/CacheService.py
+++ opsiclientd/nonfree/CacheService.py
@@ -184,7 +184,7 @@ class CacheService(threading.Thread):
 		if not productIds:
 			return True
 
-		workingWithCachedConfig = bool(configService._host in ("localhost", "127.0.0.1")) # pylint: disable=protected-access
+		workingWithCachedConfig = bool(configService.hostname.lower() in ("localhost", "127.0.0.1"))
 
 		self.initializeProductCacheService()
 
@@ -362,7 +362,7 @@ class ConfigCacheService(ServiceConnection, threading.Thread): # pylint: disable
 				raise RuntimeError("Cannot sync products: {err}") from err
 
 			try:
-				if self._configService.hostname not in ("localhost", "127.0.0.1"): # pylint: disable=protected-access
+				if self._configService.hostname.lower() not in ("localhost", "127.0.0.1"):
 					config.set(
 						'depot_server', 'master_depot_id',
 						self._configService.getDepotId(config.get('global', 'host_id')) # pylint: disable=no-member
@@ -746,7 +746,7 @@ class ProductCacheService(ServiceConnection, RepositoryObserver, threading.Threa
 				raise RuntimeError("Cannot cache config: {err}") from err
 
 			try:
-				if self._configService.hostname not in ("localhost", "127.0.0.1"): # pylint: disable=protected-access
+				if self._configService.hostname.lower() not in ("localhost", "127.0.0.1"):
 					config.set(
 						'depot_server', 'master_depot_id',
 						self._configService.getDepotId(config.get('global', 'host_id')) # pylint: disable=no-member
