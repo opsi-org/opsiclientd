405a2bd9f83a17ac14d05c2d1685efb5275990b5
Jan Schneider <j.schneider@uib.de>
2021-12-16 09:02:53 +0100

    [fix] Correct link

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -7,7 +7,7 @@ mkdir -p /usr/share/opsi-client-agent/opsiclientd
 
 [ -e /workspace/opsiclientd-dev-data/opsiclientd.conf ] || cp /workspace/opsiclientd_data/linux/opsiclientd.conf /workspace/opsiclientd-dev-data/opsiclientd.conf
 [ -e /etc/opsi-client-agent/opsiclientd.conf ] || ln -s /workspace/opsiclientd-dev-data/opsiclientd.conf /etc/opsi-client-agent/opsiclientd.conf
-[ -e /workspace/opsiclientd_data/common/static_html ] || ln -s /usr/share/opsi-client-agent/opsiclientd/static_html /workspace/opsiclientd_data/common/static_html
+[ -e /usr/share/opsi-client-agent/opsiclientd/static_html ] || ln -s /workspace/opsiclientd_data/common/static_html /usr/share/opsi-client-agent/opsiclientd/static_html
 
 if [ ! -e /usr/bin/opsi-notifier ]; then
 	cat <<EOF > /usr/bin/opsi-notifier
