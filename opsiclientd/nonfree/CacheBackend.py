db785ed9742074302b934cce51d20fe059776481
doerrer <n.doerrer@uib.de>
2023-08-24 17:12:16 +0200

    [chg] also caching products from setup_after_install

--- opsiclientd/nonfree/CacheBackend.py
+++ opsiclientd/nonfree/CacheBackend.py
@@ -413,6 +413,23 @@ class ClientCacheBackend(ConfigDataBackend, ModificationTrackingBackend):  # pyl
 		filterProductIds = []
 		if config.get("cache_service", "sync_products_with_actions_only"):
 			filterProductIds = product_ids_with_action
+			# setup_after_install is not treated as a formal dependency
+			# Adding those products here, ignoring dependencies and hoping for the best
+			# A construct big as death and twice as ugly
+			try:
+				for product in ("opsi-client-agent", "opsi-linux-client-agent", "opsi-mac-client-agent"):
+					if product in product_ids_with_action:  # one at most
+						setup_after_install_products = self._masterBackend.productPropertyState_getObjects(
+							objectId=self._clientId,
+							productId=product,
+							propertyId="setup_after_install",
+						)[0]
+						filterProductIds += [
+							sai_product for sai_product in setup_after_install_products.values if sai_product not in filterProductIds
+						]
+			except Exception as err:  # pylint: disable=broad-except
+				logger.warning("Failed to add setup_after_install products to filteredProductIds: %s", err)
+
 		# Need opsi-script PoC in cached backend for update_action_processor!
 		if filterProductIds and "opsi-script" not in filterProductIds:
 			filterProductIds.append("opsi-script")
