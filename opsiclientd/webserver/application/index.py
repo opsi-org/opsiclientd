e77ccc4b6aa66b412e8e2611a8480fa4e70324cc
Jan Schneider <j.schneider@uib.de>
2024-04-03 22:36:48 +0000

    [chg] Remove terminal link

--- opsiclientd/webserver/application/index.py
+++ opsiclientd/webserver/application/index.py
@@ -29,7 +29,6 @@ INDEX_PAGE = """<?xml version="1.0" encoding="UTF-8"?>
 		<ul>
 			<li><a target="_blank" href="/info">info page</a></li>
 			<li><a target="_blank" href="/log_viewer">log viewer</a></li>
-			<li><a target="_blank" href="/terminal">terminal</a></li>
 			<li><a target="_blank" href="/interface/opsiclientd">control interface</a></li>
 			<li><a target="_blank" href="/interface/kiosk">kiosk interface</a></li>
 			<li><a target="_blank" href="/interface/rpc">cache service interface</a></li>
