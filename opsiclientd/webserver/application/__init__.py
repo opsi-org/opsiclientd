980c17807b82296398c9b74cc9d6357949a08c34
Jan Schneider <j.schneider@uib.de>
2024-12-20 18:35:15 +0100

    [chg,pub] Cache product property values

--- opsiclientd/webserver/application/__init__.py
+++ opsiclientd/webserver/application/__init__.py
@@ -25,7 +25,7 @@ def set_opsiclientd(opsiclientd: Opsiclientd) -> None:
 	_opsiclientd = opsiclientd
 
 
-INTERFACE_PAGE = """<?xml version="1.0" encoding="UTF-8"?>
+INTERFACE_PAGE = r"""<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
