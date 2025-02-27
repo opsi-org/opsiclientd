eb8a3a078548439dcfbaaf2c20749759d691ee50
Jan Schneider <j.schneider@uib.de>
2025-02-27 14:10:03 +0100

    [chg] Revert changes

--- opsiclientd/Opsiclientd.py
+++ opsiclientd/Opsiclientd.py
@@ -995,11 +995,6 @@ class Opsiclientd(EventListener, threading.Thread):
 				skin_file = os.path.join("notifier", f"{notifier_id}.ini")
 				command = f"{command} -s {skin_file}"
 
-			if RUNNING_ON_WINDOWS:
-				# Timer interval to bring the notifier to the front
-				stay_on_top = 15 if desktop == "winlogon" else 24 * 3600
-				command = f"{command} --stay-on-top={stay_on_top}"
-
 			# Lazarus notifier does not support all IDs
 			if notifier_id == "motd":
 				notifier_id = "popup"
